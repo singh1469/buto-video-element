cb4999e7643f77cf304e8b877346a255121ac70b
hardiarl <sngh1469@gmail.com>
2015-02-11 17:15:10 +0000

    added remote=fale, removed browers property

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
                 options: {remote: false,browsers:['chrome','firefox']}
             },
             travis: {
-                options: {remote: true}
+                options: {remote: false}
             }
         }
     });
